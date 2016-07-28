bde8c854c654814cfe60de79133a7d8d34583259
Will Cass <will.cass@ovic.co.uk>
2016-07-28 12:37:10 +0100

    Moon test engage
    Going to try 1 moon orbiting 1 planet around a central point

---
+++ anims/moontest/js/moontest.js
@@ -0,0 +1,17 @@
+/**
+ * Created by Will Cass on 28-Jul-16.
+ */
+
+moontest = {
+    start: function () {
+        moontest.initBinds();
+    },
+
+    initBinds: function () {
+
+    }
+}
+
+$(document).ready(function () {
+    moontest.start();
+});
